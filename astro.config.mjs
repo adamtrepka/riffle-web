2ac474876a70718a1ae145a600bf2d20de83a2a2
user <adamtrepk@gmail.com>
2026-08-27 22:45:41 +0200

    Switch Pages to project URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://riffle.trepka.dev',
+  site: 'https://adamtrepka.github.io',
+  base: '/riffle-web',
   output: 'static',
   trailingSlash: 'always',
 })
